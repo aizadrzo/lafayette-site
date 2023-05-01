45df1da5136faa43a085f7473f2ae52744ddceba
aizadrzo <aizadridzoo@gmail.com>
2023-05-01 22:49:00 +0800

    added mega menu navbar

--- components/app-layout/footer.js
+++ components/app-layout/footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="px-4 pt-16 mx-auto lg:max-w-screen-xl lg:px-8 ">
+    <div className="px-4 pt-16 mx-auto max-w-[85rem] w-full">
       <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
         <div className="sm:col-span-2">
           <Link
--- components/app-layout/nav.js
+++ components/app-layout/nav.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const Nav = () => (
-  <header className="max-w-screen-xl mx-auto flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+  <header className="relative flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
     <nav
       className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
       aria-label="Global"
@@ -17,8 +17,8 @@ const Nav = () => (
           <button
             type="button"
             className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
-            data-hs-collapse="#navbar-with-collapse"
-            aria-controls="navbar-with-collapse"
+            data-hs-collapse="#navbar-with-mega-menu"
+            aria-controls="navbar-with-mega-menu"
             aria-label="Toggle navigation"
           >
             <svg
@@ -46,27 +46,66 @@ const Nav = () => (
         </div>
       </div>
       <div
-        id="navbar-with-collapse"
-        className="hidden basis-full grow sm:block"
+        id="navbar-with-mega-menu"
+        className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
       >
         <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
           <Link
-            className="font-medium text-gray-600 hover:text-gray-400"
+            className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
             href="/about"
           >
-            About Us
+            About
           </Link>
+          <div className="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none]">
+            <button
+              id="hs-mega-menu-basic-dr"
+              type="button"
+              className="flex items-center w-full text-gray-600 hover:text-gray-400 font-medium dark:text-gray-400 dark:hover:text-gray-500 "
+            >
+              Services
+              <svg
+                className="ml-2 w-2.5 h-2.5 text-gray-600"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                ></path>
+              </svg>
+            </button>
+
+            <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5 hidden">
+              <Link
+                className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                href="/services/auto-insurance"
+              >
+                Auto Insurance
+              </Link>
+              <Link
+                className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                href="/services/commercial-insurance"
+              >
+                Commercial Insurance
+              </Link>
+              <Link
+                className="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                href="/services/property-insurance"
+              >
+                Property Insurance
+              </Link>
+            </div>
+          </div>
           <Link
-            className="font-medium text-gray-600 hover:text-gray-400"
-            href="/services/auto-insurance"
-          >
-            Services
-          </Link>
-          <Link
-            className="font-medium text-blue-500 hover:text-blue-700"
+            className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
             href="/contact"
           >
-            Contact Us
+            Contact
           </Link>
         </div>
       </div>
