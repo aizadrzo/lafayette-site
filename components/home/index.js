0ff32f9f58e349d2fa17af5e47e6c7c0ee21decd
aizadrzo <aizadridzoo@gmail.com>
2023-05-01 22:00:04 +0800

    fix: import issue

--- components/home/index.js
+++ components/home/index.js
@@ -1,5 +1,5 @@
-import Hero from "./Hero";
-import Features from "./Features";
+import Hero from "./hero";
+import Features from "./features";
 import CTA from "./cta";
 
 export { Hero, CTA, Features };
